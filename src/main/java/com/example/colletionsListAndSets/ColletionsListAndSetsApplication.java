f7a779bc4c67ab525e9083175611e46327239a19
Алексей Шлапаков <theblack2swan@gmail.com>
2024-04-23 02:20:01 +0300

    CollectionsListAndSets

---
+++ src/main/java/com/example/colletionsListAndSets/ColletionsListAndSetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.colletionsListAndSets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColletionsListAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColletionsListAndSetsApplication.class, args);
+	}
+
+}
